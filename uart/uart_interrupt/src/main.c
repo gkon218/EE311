30b038b664c83a0805c7235167bebb2a6a936709
gkon218 <gkon218@aucklanduni.ac.nz>
2018-09-23 22:30:02 +1200

    changed to uart

--- uart/uart_interrupt/src/main.c
+++ uart/uart_interrupt/src/main.c
@@ -1,30 +1,48 @@
-// this code sets up counter0 for an 8kHz Fast PWM wave @ 16Mhz Clock
+
+#include <asf.h>
+#include <stdint.h>
+#define F_CPU 16000000UL
 
 
-#include <avr/io.h>
 
 
-int main(void)
-{
-	DDRD |= (1 << DDD6);
-	// PD6 is now an output
+#define BAUD 9600
+#define BAUD_PRE (F_CPU/BAUD/16) - 1
 
-	OCR0A = 128;
-	// set PWM for 50% duty cycle
+void uart_init(unsigned int prescaler);
+void uart_transmit (uint8_t value);
 
 
-	TCCR0A |= (1 << COM0A1);
-	// set none-inverting mode
+void uart_init(unsigned int prescaler){
+	//assign the baudrate prescaler
+	UBRR0H = (BAUD_PRE >> 8);
+	UBRR0L = BAUD_PRE;
+	
+	//enable Tx
+	UCSR0B |= (1 << TXEN0);
+	//configure for 1 stop bit and data size of 8 bits
+	UCSR0C |= (1 << UCSZ00) | (1 << UCSZ01);
+	
+	return;
+}
 
-	TCCR0A |= (1 << WGM01) | (1 << WGM00);
-	// set fast PWM Mode
+void uart_transmit (uint8_t value){
+	
+	//The function will be stuck in this while loop until the UART data register is empty
+	while (! (UCSR0A && (1<<UDRE0)) ){}
+	
+	//once the UDR is empty the value will be loaded into the UDR to be outputted into the TX pin
+	UDR0 = value;
+	
+}
 
-	TCCR0B |= (1 << CS01);
-	// set prescaler to 8 and starts PWM
-
-
-	while (1);
-	{
-		// we have a working Fast PWM
+int main(void){
+	uart_init(BAUD_PRE);
+	
+	while(1){
+		uart_transmit('a');
 	}
+	
+
+	return 0;
 }
